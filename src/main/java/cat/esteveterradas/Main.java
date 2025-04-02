a5c8024a09514723496b63fe9e4eb20163882542
Geanfranco Biagi <118919877+gbiagi@users.noreply.github.com>
2025-04-02 15:16:11 +0200

    Initial commit

---
+++ src/main/java/cat/esteveterradas/Main.java
@@ -0,0 +1,15 @@
+package cat.esteveterradas;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        
+    }
+}
